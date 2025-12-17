206ff8d282d95bc4a0e4111073a22f7db1744871
ecasado <90179693+ecasadofp@users.noreply.github.com>
2025-12-17 08:46:22 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CICD con actions</title>
+</head>
+<body>
+    <h1>CI/CD con github actions</h1>
+    <?php
+    echo 'Le voy a decir a Sergio que no tenéis ningún repo con php';
+    ?>
+</body>
+</html>
